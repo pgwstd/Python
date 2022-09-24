b7031bddc57e094dc3d2e16d86d6f213b3f06169
pgwstr <2712278005@qq.com>
2022-09-25 00:27:42 +0800

    练习

--- test_9_22.py
+++ test_9_22.py
@@ -1,9 +1,21 @@
-TempStr = input("请输入带有符号的温度值:")
+# TempStr = input("请输入带有符号的温度值:")
+# if TempStr[-1] in ['F', 'f']:
+#     C = (eval(TempStr[0:-1]) - 32) / 1.8
+#     print("转换后的摄氏温度是{:.2f}C".format(C))
+# elif TempStr[-1] in ['C', 'c']:
+#     F = 1.8 * eval(TempStr[0:-1]) + 32
+#     print("转换后的华氏温度是{:.2f}F".format(F))
+# else:
+#     print("输入有误！！！")
+
+#修改后
+TempStr = eval(input("请输入带有符号的温度值:"))
+print(TempStr)
 if TempStr[-1] in ['F', 'f']:
     C = (eval(TempStr[0:-1]) - 32) / 1.8
-    print("转换后的摄氏温度是{:.2f}C".format(C))
+    print("转换后的摄氏温度是{:d}C".format(C))
 elif TempStr[-1] in ['C', 'c']:
     F = 1.8 * eval(TempStr[0:-1]) + 32
-    print("转换后的华氏温度是{:.2f}F".format(F))
+    print("转换后的华氏温度是{:d}F".format(F))
 else:
     print("输入有误！！！")
