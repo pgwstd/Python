45c2aa9eebe008d73d4c81689ea4835cfff9e074
pgwstr <2712278005@qq.com>
2022-10-01 14:42:17 +0800

    练习

--- HomeWork/test_10_1_2.py
+++ HomeWork/test_10_1_2.py
@@ -1,5 +1,7 @@
-#画出一个等边三角形
+# 画出一个等边三角形里面又有一个等边三角形
+
 import turtle
+
 turtle.setup(650, 600, 200, 200)
 turtle.penup()
 turtle.fd(-200)
@@ -8,4 +10,12 @@ turtle.pendown()
 for i in range(3):
     turtle.fd(200)
     turtle.right(-120)
+
+turtle.fd(100)
+turtle.seth(60)
+turtle.fd(100)
+turtle.seth(180)
+turtle.fd(100)
+turtle.seth(-60)
+turtle.fd(100)
 turtle.done()
