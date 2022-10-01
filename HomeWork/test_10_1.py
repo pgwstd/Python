8fd7bf104b9df7fb558564cab2062527dc862bc9
pgwstr <2712278005@qq.com>
2022-10-01 14:15:51 +0800

    练习

--- HomeWork/test_10_1.py
+++ HomeWork/test_10_1.py
@@ -1,6 +1,11 @@
+
 import turtle
-turtle.setup(650, 400, 200, 200)
+turtle.setup(650, 600, 200, 200)
 turtle.penup()
 turtle.fd(-200)
 turtle.pendown()
-turtle.pensize(10)
+# turtle.speed(1000000000000)
+for i in range(3):
+    turtle.fd(200)
+    turtle.right(-120)
+turtle.done()
