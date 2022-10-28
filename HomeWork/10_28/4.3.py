928fd4f79a9b5ffaa7f3e77190f97a92558a0823
pgwstr <2712278005@qq.com>
2022-10-28 22:06:49 +0800

    练习

--- HomeWork/10_28/4.3.py
+++ HomeWork/10_28/4.3.py
@@ -1,13 +1,13 @@
 # 最大公约数计算。从键盘输入两个正整数，计算并输出它们的最大公约数和最小公倍数。
 
-def gcd(x, y):
+def gcd(x, y):  # 求最大公约数
     if x > y:
         x, y = y, x
     for factor in range(x, 0, -1):
         if x % factor == 0 and y % factor == 0:
             return factor
 
-def lcm(x, y):
+def lcm(x, y):  # 求最小公倍数
     return x * y // gcd(x, y)
 
 if __name__ == '__main__':
---
+++ HomeWork/10_28/4.4.py
@@ -0,0 +1,14 @@
+#猜数字游戏。在程序中预设一个0到100之间的随机数，让用户通过键盘输入一个数字，如果大于预设的数字，则显示“大了”，如果小于预设的数字，则显示“小了”，如此循环，直到用户输入的数字等于预设的数字，显示“预测N次，你猜对了”，并退出程序。
+import random
+num = random.randint(0, 100)
+count = 0
+while True:
+    guess = int(input("请输入一个数字："))
+    count += 1
+    if guess > num:
+        print("遗憾，太大了")
+    elif guess < num:
+        print("遗憾，太小了")
+    else:
+        print("预测{}次，你猜中了！".format(count))
+        break
