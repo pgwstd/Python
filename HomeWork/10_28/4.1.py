86f3cf386811a7f60780bd6a88b8554f14bd8083
pgwstr <2712278005@qq.com>
2022-10-28 21:48:45 +0800

    作业

---
+++ HomeWork/10_28/4.1.py
@@ -0,0 +1,12 @@
+#猜数字游戏。在程序中预设一个0到9之间的随机数，让用户通过键盘输入一个数字，如果大于预设的数字，则显示“大了”，如果小于预设的数字，则显示“小了”，如此循环，直到用户输入的数字等于预设的数字，显示“猜对了”，并退出程序。
+import random
+num = random.randint(0, 9)
+while True:
+    guess = int(input("请输入一个数字："))
+    if guess > num:
+        print("遗憾，太大了")
+    elif guess < num:
+        print("遗憾，太小了")
+    else:
+        print("预测{}次，你猜中了！".format(guess))
+        break
---
+++ HomeWork/10_28/4.2.py
@@ -0,0 +1,16 @@
+#统计不同字符的个数。用户从键盘输入一行字符，统计其中英文字母、空格、数字和其它字符的个数。
+s = input('请输入一行字符：')
+letters = 0 #英文字母
+space = 0   #空格
+digit = 0   #数字
+others = 0  #其它字符
+for c in s:
+    if c.isalpha(): #判断是否是字母
+        letters += 1
+    elif c.isspace():   #判断是否是空格
+        space += 1
+    elif c.isdigit():   #判断是否是数字
+        digit += 1
+    else:
+        others += 1
+print('英文字母个数：{}，空格个数：{}，数字个数：{}，其它字符个数：{}'.format(letters, space, digit, others))
