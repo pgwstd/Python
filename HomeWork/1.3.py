34f81ed7e01964e6380d05022dfa0db1f362eefd
panguangwei <2712278005@qq.com>
2022-12-08 23:08:22 +0800

    练习

--- HomeWork/1.3.py
+++ HomeWork/1.3.py
@@ -1,4 +1,4 @@
 a, b = 0, 1
 while a < 1000:
-    print(a, end = '-')
-    a, b = b, a+b
+    print(a, end='-')
+    a, b = b, a + b
--- HomeWork/1.5.py
+++ HomeWork/1.5.py
@@ -1,4 +1,4 @@
 n = 1
-for i in range(10,0,-1):
-     n = (n+1)<<1
+for i in range(10, 0, -1):
+    n = (n + 1) << 1
 print(n)
